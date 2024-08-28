4bf4c4bc53ec2094b1337f05ac2fef02595c2a16
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 10:52:12 +0700

    Add files via upload

---
+++ bagasproject1/src/main/java/com/mycompany/bagasproject1/Bagasproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.bagasproject1;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Bagasproject1 {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
