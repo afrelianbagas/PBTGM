b4dab396b494f2c07d877e74364f7b05c4b6a117
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 15:14:23 +0700

    Add files via upload

---
+++ Pertemuan6Soal2/src/main/java/com/mycompany/pertemuan6soal2/Pertemuan6Soal2.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6soal2;
+import java.util.Scanner;
+/**
+ *
+ * @author Afrelian
+ */
+public class Pertemuan6Soal2 {
+
+     public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+        // Input dari pengguna
+        System.out.print("NIM ? : ");
+        String nim = scanner.nextLine();
+        
+        System.out.print("NAMA ? : ");
+        String nama = scanner.nextLine();
+        
+        System.out.print("NILAI ? : ");
+        int nilai = scanner.nextInt();
+
+        // Menentukan grade berdasarkan nilai
+        String grade;
+        String keterangan;
+        
+        if (nilai >= 80) {
+            grade = "A";
+            keterangan = "Lulus";
+        } else if (nilai >= 70) {
+            grade = "B";
+            keterangan = "Lulus";
+        } else if (nilai >= 60) {
+            grade = "C";
+            keterangan = "Lulus";
+        } else if (nilai >= 50) {
+            grade = "D";
+            keterangan = "Tidak Lulus";
+        } else {
+            grade = "E";
+            keterangan = "Tidak Lulus";
+        }
+
+        // Menampilkan hasil
+      
+        System.out.println("grade " + grade);
+        System.out.println("Keterangan : " + keterangan);
+    }
+}
