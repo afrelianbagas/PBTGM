ff6d4a98f3e7858180a019f1c7460464ed5879a1
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 15:12:01 +0700

    Add files via upload

---
+++ Pertemuan2Soal2/src/main/java/com/mycompany/pertemuan2soal2/Pertemuan2Soal2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2soal2;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Pertemuan2Soal2 {
+
+   public static String Makanan;
+//Membuat variable tanpa Value/Nilai pada class staticvariable 
+    public static final String Minuman = "Jus Jambu";
+
+    public static void main(String[] args) {
+
+        
+// TODO code application logic here 
+            Makanan = "Ayam Geprek ";//Mengisi Value pada variable 
+            System.out.println("Makanan Favorit saya : " + Makanan);//Memanggil Variable Hobi 
+            System.out.println("Minuman Favorit Saya : " + Minuman);//Memanggil Variable Hewan
+    }
+}
