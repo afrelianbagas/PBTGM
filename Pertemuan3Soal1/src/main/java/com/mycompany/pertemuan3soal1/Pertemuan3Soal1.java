222a0f34b40c9743e37869e5ccd320f8772ac848
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 15:12:19 +0700

    Add files via upload

---
+++ Pertemuan3Soal1/src/main/java/com/mycompany/pertemuan3soal1/Pertemuan3Soal1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Pertemuan3Soal1 {
+
+    public static void main(String[] args) {
+        short angka;
+        angka = 9;
+        System.out.println(angka);
+
+        float fl = 8.99f;
+        System.out.println(fl);
+
+        char cha = 'A';
+        System.out.println(cha);
+
+        boolean b = false;
+        System.out.println(b);
+
+        System.out.println("Hello World!");
+    }
+}
