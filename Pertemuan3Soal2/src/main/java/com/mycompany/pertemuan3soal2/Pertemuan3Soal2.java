9d57c317d983f2a2251002f1478c3bb1133be070
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 15:12:38 +0700

    Add files via upload

---
+++ Pertemuan3Soal2/src/main/java/com/mycompany/pertemuan3soal2/Pertemuan3Soal2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal2;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Pertemuan3Soal2 {
+
+   public static String mahasiswa = "Andika Jaya Putra";
+    public static String kelas = "01SIFE020";
+
+    public static void main(String[] args) {
+
+        long a = 2110117000L;
+        float f = 19.5f;
+        double d = 1.72d;
+
+        System.out.println("Nim              : " + a);
+        System.out.println("Nama Mahasiswa   : " + mahasiswa);
+        System.out.println("Kelas            : " + kelas);
+        System.out.println("Usia             : " + f);
+        System.out.println("Tinggi           : " + d);
+    }
+}
