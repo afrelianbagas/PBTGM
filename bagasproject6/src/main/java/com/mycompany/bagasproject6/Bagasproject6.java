c1e25ce242afb20a3b2e919bc2d41fb3f67f8536
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-08-28 10:54:18 +0700

    Add files via upload

---
+++ bagasproject6/src/main/java/com/mycompany/bagasproject6/Bagasproject6.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bagasproject6;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Bagasproject6 {
+
+    public static void main(String[] args) {
+       int w = 5;
+       int x = 6;
+       int y = y = 8 - x++; 
+       int z = 8 - ++w;
+       
+       System.out.println("Nilai w= " + w);
+       System.out.println("Nilai x= " + x);
+       System.out.println("Nilai y= " + y);
+       System.out.println("Nilai z= " + z);
+    }
+}
